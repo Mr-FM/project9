c3c848855ea37c65d9e60fa10c58a66e85a96bb9
nicolasnomal <71435226+nicolasnomal@users.noreply.github.com>
2020-11-07 00:14:48 +0100

    Update project9.sql

--- project9.sql
+++ project9.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2020 at 03:15 PM
+-- Generation Time: Nov 07, 2020 at 12:12 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -76,11 +76,22 @@ CREATE TABLE `movies` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `releasedate` year(4) NOT NULL,
-  `categoryid` int(11) NOT NULL,
   `poster` varchar(255) NOT NULL,
   `synopsis` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movies`
+--
+
+INSERT INTO `movies` (`id`, `title`, `releasedate`, `poster`, `synopsis`, `path`) VALUES
+(1, 'The Dark Knight', 2008, 'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg', 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 'darkknight.avi'),
+(2, 'Gladiator', 2000, 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg', 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 'gladiator.avi\r\n'),
+(3, 'Borat: Subsequent Moviefilm', 2020, 'https://m.media-amazon.com/images/M/MV5BZmY2ZmM5YTktZThiOC00YjEzLTg4YTctMDAxYWEyZmEyZDlhXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_UX182_CR0,0,182,268_AL_.jpg', 'Follow-up film to the 2006 comedy centering on the real-life adventures of a fictional Kazakh television journalist named Borat.', 'BORAT2.avi'),
+(4, '\r\nLes Sept de Chicago ', 2020, 'https://m.media-amazon.com/images/M/MV5BYjYzOGE1MjUtODgyMy00ZDAxLTljYTgtNzk0Njg2YWQwMTZhXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg', 'The story of 7 people on trial stemming from various charges surrounding the uprising at the 1968 Democratic National Convention in Chicago, Illinois.', 'les7dechicago.mp4'),
+(5, 'Tenet', 2020, 'https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_UX182_CR0,0,182,268_AL_.jpg', 'Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.', 'tenetmovie.avi'),
+(6, 'Freaky', 2020, 'https://m.media-amazon.com/images/M/MV5BMTRkYmJlY2ItNmFlZi00OWVhLTg1ZTctOGE0MjM5ZGMwMmY4XkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_UX182_CR0,0,182,268_AL_.jpg', 'After swapping bodies with a deranged serial killer, a young girl in high school discovers she has less than 24 hours before the change becomes permanent.', 'FREAKY.mpg');
 
 -- --------------------------------------------------------
 
@@ -122,6 +133,16 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `auth`) VALUES
+(8, 'Nicolas', 'NOMAL', 'nicolasnomalpro@gmail.com', '12345678', 'admin'),
+(9, 'Nicolas', 'NOMAL', 'nicoss9pro@gmail.com', '1212121212', 'admin'),
+(10, 'Nicolas', 'NOMAL', 'simon@gmail.com', '123456789', 'user'),
+(11, 'simon', 'bertrand', 'nicolasnomalpro@gmail.com', '123456789', 'user');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -142,7 +163,6 @@ ALTER TABLE `actorsandmovies`
 -- Indexes for table `categoriesandmovies`
 --
 ALTER TABLE `categoriesandmovies`
-  ADD KEY `c3` (`categoryid`),
   ADD KEY `c4` (`moviesid`);
 
 --
@@ -155,8 +175,7 @@ ALTER TABLE `category`
 -- Indexes for table `movies`
 --
 ALTER TABLE `movies`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `c8` (`categoryid`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `moviesandplaylists`
@@ -198,7 +217,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `movies`
 --
 ALTER TABLE `movies`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `playlists`
@@ -210,7 +229,7 @@ ALTER TABLE `playlists`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
@@ -229,12 +248,6 @@ ALTER TABLE `actorsandmovies`
 ALTER TABLE `categoriesandmovies`
   ADD CONSTRAINT `c3` FOREIGN KEY (`categoryid`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `c4` FOREIGN KEY (`moviesid`) REFERENCES `movies` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `movies`
---
-ALTER TABLE `movies`
-  ADD CONSTRAINT `c8` FOREIGN KEY (`categoryid`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `moviesandplaylists`
