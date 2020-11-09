4c5cf58cb1b3ce7ca51af504946e25f0862c80fb
Mr-FM <71437182+Mr-FM@users.noreply.github.com>
2020-11-09 16:34:16 +0100

    Create search.php
    bouton (nav) (dans un fichier search.php)

---
+++ search.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Links for the fonts -->
+
+    <!--My css at the bottom -->
+    <link rel="stylesheet" href="style/styles.css" />
+    <title>Search</title>
+</head>
+<header>
+    <div>
+        <nav>
+            <ul>
+                <li><a href="./home.php">Accueil</a></li>
+                <li><a href="./categories.php">Catégories</a></li>
+                <li><a href="./search.php">Search</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div>
+        <nav id="top-navigation">
+            <form action="" method="POST">
+                <input type="text" name="search" placeholder="Search">
+
+                <input type="submit" name="submitBtn" value="OK">
+            </form>
+        </nav>
+        </hr>
+    </div>
+</header>
+
+<body>
+
+</body>
+
+</html>
