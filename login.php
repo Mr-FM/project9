e9b2023edab20608c41c9b9da9586ddab0e72ed7
nicolasnomal <71435226+nicolasnomal@users.noreply.github.com>
2020-11-10 09:52:08 +0100

    hash in register  and login

--- login.php
+++ login.php
@@ -1,72 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-<header>
-
-    <nav>
-        <ul>
-            <li><a href="./register.php">Register</a></li>
-            <li><a href="./login.php">Login</a></li>
-            <li><a href="./account.php">Account</a></li>
-        </ul>
-    </nav>
-
-</header>
-
-<body>
-
-    <h2>Sign-in</h2>
-    <form action="" method="POST">
-        <input type="Email" name="email" placeholder="Your Email"><br>
-        <input type="text" name="password" placeholder="Your password"><br>
-        <input type="submit" name="submitBtn" value="signin">
-    </form>
-
-    <?php
-
-    require_once 'database.php';
-    //connect to the DATABASE project9
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    //echo 'Connexion successfull <br>';
-    $db_name = 'project9';
-    $db_found = mysqli_select_db($conn, $db_name);
-
-    if ($db_found) {
-        //Make sure i clicked the button
-        if (isset($_POST['submitBtn'])) {
-            //Get info from the form(and remove white spaces)
-            $mail = trim($_POST['email']);
-            $password = trim($_POST['password']);
-
-            //execute QUERY 
-            $sql_query = "SELECT * FROM users WHERE email = '$email' AND password = '$password' ";
-
-            //execute the query
-            $currentUser = mysqli_query($conn, $sql_query);
-
-            // fetch results as associative array
-            $_SESSION['users'] = mysqli_fetch_assoc($currentUser);
+<body> 
+     <form action="" method="POST">
+       <input type="email" name="email" placeholder="Email">
+       <br>
+       <input type="password" name="password" placeholder="Mot de passe">
+       <br>
+       <input type="submit" name="submitBtn" id="butn">
+     </form>
 
 
-            $message = 'you are not allow on the website';
+<?php
 
-            //echo $mail . '/' . $password . '<br>';
+if(isset($_POST['submitBtn']))
+{
 
-            // Check if there is a match
-            if ($email == trim($email) && $password == trim($password)) {
-                $message = 'You are online';
-                echo $message;
-            } else {
-                echo 'Something went wrong try again.';
-            }
-        }
+require_once 'database.php';
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$db_name = 'project9';
+$db_found = mysqli_select_db($conn, $db_name);
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+
+if(empty($email)||empty($password))
+   {
+   echo 'ERREUR, merci de saisir email et password.';
+   }
+   else
+   {
+     if ($db_found)
+     {
+      $query = "SELECT * FROM users WHERE '$email' = email";
+      $result_query = mysqli_query($conn, $query);
+      $result_array = mysqli_fetch_assoc($result_query);
+      $hashPass = $result_array['password'];
+      if($result_array==null)
+      {
+       echo 'email invalid';
+      }
+      elseif(!password_verify($password, $hashPass))
+      {
+        echo 'password invalid';
+      }
+      else
+      {
+        echo 'Bienvenue '.$result_array['firstname'].'!';      
+      }
+     }
+     else
+     {
+      echo 'database error';
+     }
+    
     }
-    ?>
-</body>
+}
+?>
 
+</body>
 </html>
---
+++ register.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body> 
+     <form action="" method="POST">
+       <input type="text" name="firstname" placeholder="Prénom">
+       <br>
+       <input type="text" name="lastname" placeholder="Nom">
+       <br>
+       <input type="email" name="email" placeholder="Email">
+       <br>
+       <input type="password" name="password" placeholder="Mot de passe">
+       <br>
+       <input type="password" name="cPassword" placeholder="Confirmez mot de passe">
+       <br>
+       <select name="auth" id="auth">
+         <option value="user">User</option>
+         <option value="admin">Admin</option>
+       </select>
+       <br>
+       <input type="submit" name="submitBtn" id="butn">
+     </form>
+
+
+<?php
+
+if(isset($_POST['submitBtn'])){
+
+require_once 'database.php';
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+$db_name = 'project9';
+$db_found = mysqli_select_db($conn, $db_name);
+
+$firstname = $_POST['firstname'];
+
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$cPassword = $_POST['cPassword'];
+$auth = $_POST['auth'];
+
+//check nom et prénom non vides
+if(empty($firstname)||empty($lastname))
+   {
+   echo 'ERREUR, vous devez taper le nom et le prénom.';
+   }
+ elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) //check mail
+   {
+    echo 'ERREUR, votre email est invalide!';
+   }
+  elseif(strlen($password)<8) //check mot de passe > 8 caractères
+   {
+     echo 'ERREUR, votre mot de passe est trop court (8 caractères minimum)';
+   }
+   elseif($password!=$cPassword)//check mot de passe == confirmation de mot de passe 
+   {
+     echo 'ERREUR, vos mots de passe sont différents!';
+   }
+   elseif ($db_found)
+   {
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $query = "INSERT INTO users(firstname,lastname,email,password,auth) VALUES('$firstname','$lastname','$email','$password','$auth')";
+    $result_query = mysqli_query($conn, $query);
+    echo 'Inscription OK! Bienvenue '.$firstname.'!';
+   }
+   else
+   {
+    echo 'database error';
+   }
+}
+?>
+
+</body>
+</html>
