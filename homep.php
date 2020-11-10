aead1d6c561310f49d8ec249a1124ba508915dfd
nicolasnomal <71435226+nicolasnomal@users.noreply.github.com>
2020-11-10 16:53:05 +0100

    filter by cat

--- homep.php
+++ homep.php
@@ -7,11 +7,14 @@
     <title>Home</title>
 </head>
 <body>
+    <?php
+      include'menu.html';
+    ?>
     <h1>Titre</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saefdgfgpe, perferendis modi dolor deleniti aliquam ab molestias odio! Animi ipsam soluta beatae iure, accusantium perspiciatis adipisci recusandae qui. Esse, aperiam voluptas?</p>
-    <form action="" method="POST">
+    <!-- <form action="" method="POST">
     <input type="search" name="search" placeholder="Type a movie name" id="search">
-    </form>
+    </form> -->
     <?php
 
     require_once 'database.php';
@@ -34,6 +37,25 @@
         
       
     ?>
+    <form action="" method="POST">
+       <select name="filter" id="filter">
+         <option value="0" default>Choose category</option>
+       <?php
+        require_once 'database.php';
+        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        $db_name = 'project9';
+        $db_found = mysqli_select_db($conn, $db_name);
+        $query = "SELECT name,id FROM category";
+        $result = mysqli_query($conn, $query);
+        while ($categories = mysqli_fetch_assoc($result)) 
+       {
+        echo '<option value="'.$categories['id'].'">'; 
+        echo $categories['name'];
+        echo '</option>';
+       } 
+      ?>
+       </select>  
+    </form>
     <div id="movieZone">
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -41,47 +63,30 @@
     <script>
         // Wait for the dom to be loaded/ready before executing javascript
         $(function () {
-            $.ajax({
-                    url: 'homepage2.php',
-                    method: 'post',
-                    data: $('input').serialize()
-                }).done(function(result) {
-                    // If ajax call worked
-                    console.log('SUCCESS : ' + result);
-                    $("#movieZone").html(result);
-                                     
-                                       
-                    //document.write(result);
-                                    
-                }).fail(function (result) {
-                    // If AJAX failed
-                    console.log('AJAX ERROR');
-                    
-                });
+                       $.ajax({ url: 'homepage2.php', method: 'post', data: $('input').serialize()})
+                        .done(function(result){console.log('SUCCESS : ' + result);$("#movieZone").html(result);})
+                        .fail(function (result){console.log('AJAX ERROR');});
             
-            $("#search").keyup(function (e) {
-                e.preventDefault();
-
-                $.ajax({
-                    url: 'homepage2.php',
-                    method: 'post',
-                    data: $('input').serialize()
-                }).done(function(result) {
-                    // If ajax call worked
-                    console.log('SUCCESS : ' + result);
-                    $("#movieZone").html(result);
+                       $("#search").keyup(function (e) 
+                        {  
+                        e.preventDefault();
+                        $.ajax({ url: 'homepage2.php', method: 'post', data: $('input').serialize()})
+                        .done(function(result) { console.log('SUCCESS : ' + result); $("#movieZone").html(result);})
+                        .fail(function (result) {console.log('AJAX ERROR');}); 
+                        }); 
+                        
+                       $("#filter").change(function (e) 
+                        {  
+                        e.preventDefault();
+                        $.ajax({ url: 'filtermovies.php', method: 'post', data: $('#filter').serialize()})
+                        .done(function(result) { console.log('SUCCESS : ' + result); $("#movieZone").html(result);})
+                        .fail(function (result) {console.log('AJAX ERROR');});
+                        });    
+                              
+                       
+                      });    
+          
                     
-                    
-                                       
-                    //document.write(result);
-                                    
-                }).fail(function (result) {
-                    // If AJAX failed
-                    console.log('AJAX ERROR');
-                    
-                });
-            });
-        });
     </script>
 </body>
 </html>
